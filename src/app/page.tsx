d58c7f3359daf685daf66307b1f229d2c18edee2
rahulbanik98 <59626411+rahulbanik98@users.noreply.github.com>
2024-03-03 12:52:04 +0530

    use client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React, { FC, useEffect, useState } from "react";
 import {
   Dynamicbodycontainer,
@@ -14,7 +16,6 @@ import {
   getDayOrNightIcon,
   metersToKilometers,
 } from "@/utils/Utilstempfunctions";
-import { fromUnixTime } from "date-fns";
 import { placeAtom, pullData } from "./atom";
 import { useAtom } from "jotai";
 
